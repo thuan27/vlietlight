e8ee1c6740f519bbe0292338c93f21826478f602
IT department <dataxang@gmail.com>
2020-06-16 22:21:47 +0700

    test in web not use /apps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ export function tokenGetter() {
 
 const appRoutes: Routes = [
     {
-        path        : 'apps',
+        path        : '',
         loadChildren: './main/content/apps/apps.module#FuseAppsModule'
     },
     {
