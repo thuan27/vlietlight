b8f529445842026b91b05eabfc3e2b950d9cb053
IT department <dataxang@gmail.com>
2020-07-08 11:35:41 +0700

    Update to test Logout

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -18,7 +18,7 @@ import { FuseLoginFormDialogComponent } from '@fuse/components/login-form/login-
 export class FuseMainComponent implements OnDestroy
 {
     //private intervalCheckVersion = 84600000;
-    private intervalCheckVersion = 300000;
+    private intervalCheckVersion = 600000;
     private intervalCheck;
     onConfigChanged: Subscription;
     fuseSettings: any;
