fc08f45f61c0cbba68fec274528535a9cce9574a
Thuan Le Quang <lequangthuan271995@gmail.com>
2020-12-07 22:17:12 +0700

    landing

--- src/app/main/toolbar/toolbar.component.ts
+++ src/app/main/toolbar/toolbar.component.ts
@@ -48,6 +48,7 @@ export class FuseToolbarComponent
       this.check = localStorage.getItem(environment.token) != null;
         if (!this.jwtHelper.isTokenExpired(this.getToken())) {
             this.profile = this.jwtHelper.decodeToken(localStorage.getItem(environment.token));
+            console.log(this.profile)
         }
         else {
             this.profile['fullname'] = '';
